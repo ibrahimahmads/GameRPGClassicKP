434484b442a4ef339cdad342a76366db9f078d4e
Ibrahim Ahmad <ibrahimahmad593@gmail.com>
2024-09-18 02:01:13 +0700

    player attack dan animasi

--- Game-RPG-Classic_KP/Assets/Scripts/PlayerController.cs
+++ Game-RPG-Classic_KP/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 2f;
+    public LayerMask enemyLayers;
+    public int damage = 40;
 
     private void Awake()
     {
@@ -26,6 +30,7 @@ public class PlayerController : MonoBehaviour
     {
         PlayerInput();
         Animation();
+        Attack();
     }
 
     private void FixedUpdate()
@@ -57,4 +62,63 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("IsMoving", false);
         }
     }
+
+    private void Attack()
+    {
+        if (Input.GetButtonDown("Fire1")) // Tombol serangan, misalnya "Fire1"
+        {
+            // Tentukan arah serangan berdasarkan input dan update posisi attackPoint
+            Vector2 attackDirection = Vector2.down;
+
+            if (movement.x > 0) // Kanan
+            {
+                attackDirection = Vector2.right;
+                animator.SetFloat("AttDirection", 2);
+                attackPoint.localPosition = attackDirection * 0.7f;
+            }
+            else if (movement.x < 0) // Kiri
+            {
+                attackDirection = Vector2.left;
+                animator.SetFloat("AttDirection", 3);
+                attackPoint.localPosition = attackDirection * 0.7f;
+            }
+            else if (movement.y > 0) // Atas
+            {
+                attackDirection = Vector2.up;
+                animator.SetFloat("AttDirection", 1);
+                attackPoint.localPosition = attackDirection * 0.3f;
+            }
+            else if (movement.y < 0) // Bawah
+            {
+                attackDirection = Vector2.down;
+                animator.SetFloat("AttDirection", 0);
+                attackPoint.localPosition = attackDirection * 1.3f;
+            }
+
+            // Pindahkan attackPoint ke arah yang sesuai
+             
+
+            // Trigger animasi serangan
+            animator.SetTrigger("Attack");
+
+            // Detect enemies in range of attack
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+            // Berikan damage ke musuh yang terkena
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("Enemy hit: " + enemy.name);
+                enemy.GetComponent<EnemyHealth>()?.TakeDamage(damage); // Pastikan enemy punya script untuk menerima damage
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
 }
