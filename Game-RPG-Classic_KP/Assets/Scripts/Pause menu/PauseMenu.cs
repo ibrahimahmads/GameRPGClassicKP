1c784f157dd3602a04a7b665d7036e3df851b632
ibrahimahmads <ibrahimahmad593@gmail.com>
2024-10-08 15:38:40 +0700

    pause menu

--- Game-RPG-Classic_KP/Assets/Scripts/Pause menu/PauseMenu.cs
+++ Game-RPG-Classic_KP/Assets/Scripts/Pause menu/PauseMenu.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections; 
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -21,29 +23,43 @@ public class PauseMenu : MonoBehaviour
             }
             else
             {
-                Pause();
+                StartCoroutine(PauseCoroutine());
             }
         }
     }
 
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
+        animator.Play("pause hide");
+        Invoke("DeactivatePauseMenu", .4f);
         Time.timeScale = 1f;  // Resume the game time
         isPaused = false;
     }
 
-    void Pause()
+    IEnumerator PauseCoroutine()
     { 
-        pauseMenuUI.SetActive(true);
+        pauseMenuUI.SetActive(true);  // Aktifkan panel sebelum animasi berjalan
         animator.Play("pause_show");
-        Time.timeScale = 1f;  // Pause the game time
+
+        yield return new WaitForSecondsRealtime(1.08f);  // Menunggu animasi selesai di real-time (tidak terpengaruh Time.timeScale)
+        
+        Time.timeScale = 0f;  // Pause game setelah animasi selesai
         isPaused = true;
+    }
+
+    void DeactivatePauseMenu()
+    {
+        pauseMenuUI.SetActive(false);  // Nonaktifkan panel setelah animasi selesai
     }
 
     public void QuitGame()
     {
         Debug.Log("Quitting game...");
         Application.Quit();
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
