9d79121d53edd0b6ad9e4f9bea45b129b0c7c95d
Ibrahim Ahmad <ibrahimahmad593@gmail.com>
2024-10-30 02:33:45 +0700

    design vilage

--- Game-RPG-Classic_KP/Assets/Scripts/DynamicSorting.cs
+++ Game-RPG-Classic_KP/Assets/Scripts/DynamicSorting.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class DynamicSorting : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    public float updateRadius = 10f;       // Radius jarak untuk update sorting order
+    private Transform playerTransform;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerTransform = player.transform;
+        }
+        else
+        {
+            Debug.LogError("Player object not found. Make sure the player has a tag 'Player'.");
+        }
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (spriteRenderer != null)
+        if (playerTransform != null && IsWithinRadius())
         {
             spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
         }
+    }
+
+    private bool IsWithinRadius()
+    {
+        float distance = Vector2.Distance(transform.position, playerTransform.position);
+        return distance <= updateRadius;
     }
 }
